98c7e1e84d7039d4743e7b9ebf55d204357a6148
Deepak Prasad <deepakprasad224@gmail.com>
2025-05-29 17:45:36 +0530

    side menu update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,3 +1,8 @@
+<style>
+    hr{
+        background-color: #ffffff;
+    }
+</style>
 @if (Auth::user()->user_type == 1)
     {{-- for admin side bar --}}
 
@@ -7,7 +12,7 @@
         <!-- Brand Logo -->
         <a href="{{ route('root') }}" class="brand-link">
             <img src="{{ url('/medspa.png') }}" alt="Forever Medspa" class="brand-image img-circle elevation-3"
-                style="opacity: .8" onerror="this.onerror=null; this.src='{{url('/No_Image_Available.jpg')}}';">
+                style="opacity: .8" onerror="this.onerror=null; this.src='{{ url('/No_Image_Available.jpg') }}';">
             <span class="brand-text font-weight-light">Forever Medspa</span>
         </a>
 
@@ -17,7 +22,8 @@
             <div class="user-panel mt-3 pb-3 mb-3 d-flex">
                 <div class="image">
                     <img src="@if (Auth::user()->avatar != '') {{ URL::asset(Auth::user()->avatar) }}@else{{ URL::asset('medspa.png') }} @endif"
-                        class="img-circle elevation-2" alt="User Image" onerror="this.onerror=null; this.src='{{url('/No_Image_Available.jpg')}}';">
+                        class="img-circle elevation-2" alt="User Image"
+                        onerror="this.onerror=null; this.src='{{ url('/No_Image_Available.jpg') }}';">
                 </div>
                 <div class="info">
                     <a href="#" class="d-block"> {{ Auth::user()->name }}</a>
@@ -30,7 +36,7 @@
                     <input class="form-control form-control-sidebar" type="search" placeholder="Search"
                         aria-label="Search">
                     <div class="input-group-append">
-                        <button  class="btn btn-block btn-sidebar">
+                        <button class="btn btn-block btn-sidebar">
                             <i class="fas fa-search fa-fw"></i>
                         </button>
                     </div>
@@ -43,9 +49,9 @@
                     data-accordion="false">
                     <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
+                    <li class="nav-header"><i class="nav-icon fas fa-tachometer-alt fa-1x mr-2"></i>Dashboard</li>
                     <li class="nav-item menu-open">
                         <a href="#" class="nav-link active">
-                            <i class="nav-icon fas fa-tachometer-alt"></i>
                             <p>
                                 Dashboard
                                 <i class="right fas fa-angle-left"></i>
@@ -66,87 +72,76 @@
                             </li>
                         </ul>
                     </li>
+                    <li class="nav-header">
+                        <hr>
+                    </li>
                     {{-- end Dashboard --}}
-                     
-                    {{--  For Manage --}}
-                    <li class="nav-header">Manage</li>
+
+                    {{--  For Patient --}}
+                    <li class="nav-header"><i class="fas fa-hospital-user fa-1x mr-2"></i> Patient Management </li>
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fas fa-solid fa-gift"></i>
+                            {{-- <i class="nav-icon fas fa-solid fa-gift"></i> --}}
                             <p>
-                                Manage
+                                Patient
                                 <i class="fas fa-angle-left right"></i>
-                                <span class="badge badge-info right">5</span>
+
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-heartbeat"></i>
-                                        <p>
-                                            Giftcards
-                                            <i class="fas fa-angle-left right"></i>
-                                            <span class="badge badge-info right">2</span>
-                                        </p>
-                                    </a>
-                                    <ul class="nav nav-treeview">
-                                        <li class="nav-item">
-                                            <a href="{{ route('cardgenerated-list') }}" class="nav-link">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p>Giftcard Orders</p>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="{{ route('giftcardredeem-view') }}" class="nav-link">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p>Giftcard Redeem</p>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#" class="nav-link">
-                                        <i class="nav-icon fas fa-heartbeat"></i>
-                                        <p>
-                                            Services
-                                            <i class="fas fa-angle-left right"></i>
-                                            <span class="badge badge-info right">2</span>
-                                        </p>
-                                    </a>
-                                    <ul class="nav nav-treeview">
-                                        <li class="nav-item">
-                                            <a href="{{ route('service-order-history.index') }}" class="nav-link">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p>Service Orders</p>
-                                            </a>
-                                        </li>
-                                        <li class="nav-item">
-                                            <a href="{{ route('service-redeem-view') }}" class="nav-link">
-                                                <i class="far fa-circle nav-icon"></i>
-                                                <p> Service Redeem</p>
-                                            </a>
-                                        </li>
-                                    </ul>
-                                </li>
                             <li class="nav-item">
                                 <a href="{{ route('patient.index') }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
-                                    <p>  Patient List</p>
+                                    <p> Patient List</p>
                                 </a>
                             </li>
-                                                      
-                           
                         </ul>
                     </li>
 
+                    <li class="nav-header">
+                        <hr>
+                    </li>
+                    {{--  For Manage --}}
+                    <li class="nav-header"><i class="fas fa-cart-arrow-down fa-1x mr-2"></i> Website Orders</li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                           
+                            <p>
+                                Orders
+                                <i class="fas fa-angle-left right"></i>
+                                <span class="badge badge-info right">2</span>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('service-order-history.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Service Orders</p>
+                                </a>
+                            </li>
 
+                        </ul>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('cardgenerated-list') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Giftcard Orders</p>
+                                </a>
+                            </li>
 
+                        </ul>
+                    </li>
+
+                    <li class="nav-header">
+                        <hr>
+                    </li>
+                    <li class="nav-header"><i class="fab fa-centos fa-1x mr-2"></i>In Clinic Operations</li>
                     {{-- <li class="nav-header">In Clinic</li> --}}
                     <li class="nav-item">
                         <a href="#" class="nav-link">
-                            <i class="nav-icon fa fa-user-md"></i>
+                            <i class="fas fa-capsules fa-1x mr-2"></i>
                             <p>
-                                In Clinic
+                                Sale
                                 <i class="fas fa-angle-left right"></i>
                                 <span class="badge badge-info right">2</span>
                             </p>
@@ -158,28 +153,65 @@
                                     <p>Sale Gift Card</p>
                                 </a>
                             </li>
-                            
-                            
+
+
                             <li class="nav-item">
                                 <a href="{{ route('product.index') }}" class="nav-link">
                                     <i class="far fa-circle nav-icon"></i>
                                     <p>Service & Deals Sale</p>
                                 </a>
                             </li>
-                            
+                            <li class="nav-item">
+                                <a href="{{ route('program.index') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>
+                                        Program Sale
+                                    </p>
+                                </a>
+                            </li>
+
                         </ul>
                     </li>
 
+                    <li class="nav-item">
+                        <a href="#" class="nav-link">
+                           <i class="fas fa-users-cog fa-1x mr-2"></i>
+                            <p>
+                                Redeem Section
+                                <i class="fas fa-angle-left right"></i>
+                                <span class="badge badge-info right">2</span>
+                            </p>
+                        </a>
+                        <ul class="nav nav-treeview">
+                            <li class="nav-item">
+                                <a href="{{ route('giftcardredeem-view') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p>Giftcard Redeem</p>
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="{{ route('service-redeem-view') }}" class="nav-link">
+                                    <i class="far fa-circle nav-icon"></i>
+                                    <p> Service Redeem</p>
+                                </a>
+                            </li>
 
+                        </ul>
+                    </li>
+
+                    <li class="nav-header">
+                        <hr>
+                    </li>
+                    <li class="nav-header"><i class="fas fa-sliders-h fa-1x mr-2"></i>MISCELLANEOUS</li>
                     {{--  For Create  --}}
                     {{-- <li class="nav-header">Create</li> --}}
                     <li class="nav-item">
                         <a href="#" class="nav-link">
                             <i class="nav-icon fa fa-user-md"></i>
                             <p>
-                                Create
+                                Entity Management 
                                 <i class="fas fa-angle-left right"></i>
-                                <span class="badge badge-info right">10</span>
+                               
                             </p>
                         </a>
                         <ul class="nav nav-treeview">
@@ -189,7 +221,7 @@
                                     <p>
                                         Program Management
                                         <i class="fas fa-angle-left right"></i>
-                                        <span class="badge badge-info right">2</span>
+                                      
                                     </p>
                                 </a>
                                 <ul class="nav nav-treeview">
@@ -209,7 +241,7 @@
                                             </p>
                                         </a>
                                     </li>
-                                    
+
                                 </ul>
                             </li>
 
@@ -218,7 +250,7 @@
                                     <i class="nav-icon fas fa-calendar-alt"></i>
                                     <p>
                                         Deals Management
-    
+
                                     </p>
                                 </a>
                             </li>
@@ -238,7 +270,7 @@
                                     </p>
                                 </a>
                             </li>
-                           
+
 
                             <li class="nav-item">
                                 <a href="{{ route('terms.index') }}" class="nav-link">
@@ -249,7 +281,7 @@
                                 </a>
                             </li>
 
-                          
+
                             <li class="nav-item">
                                 <a href="{{ route('coupon.index') }}" class="nav-link">
                                     <i class="nav-icon fa fa-barcode"></i>
@@ -267,7 +299,7 @@
                                     </p>
                                 </a>
                             </li> --}}
-        
+
                             <li class="nav-item">
                                 <a href="{{ route('email-template.index') }}" class="nav-link">
                                     <i class="nav-icon far fa-envelope"></i>
@@ -284,26 +316,32 @@
                                     </p>
                                 </a>
                             </li>
-                            
+
                         </ul>
                     </li>
-                    
-                    
-                    
-                    
+
+                    <li class="nav-header">
+                        <hr>
+                    </li>
+                    <li class="nav-header"><i class="fas fa-cog fa-1x mr-2"></i>Settings</li>
+
+
                     <li class="nav-item">
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                            style="display: none;">
                             @csrf
                         </form>
-                     
-                        <a href="#"  onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="nav-link">
+
+                        <a href="#"
+                            onclick="event.preventDefault(); document.getElementById('logout-form').submit();"
+                            class="nav-link">
                             <i class="nav-icon fa fa-power-off"></i>
                             <p>
                                 Logout
                             </p>
                         </a>
                     </li>
-                   
+
 
 
                 </ul>
